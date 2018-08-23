2527789450674ca7ea9cf5b33c4a60185ceef122
Dmitry <dmitry.osichkin@gmail.com>
2018-08-23 20:28:28 +0300

    23.08.2018 - calculate 2 or 3 operations

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,14 +1,63 @@
 package calculator;
 
+import java.util.function.DoubleBinaryOperator;
+
 /**
- * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
+ * @author Dimitry Osichkin
  * @version 1.0
  * @since 1.0
  */
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        double a = Double.parseDouble(expression[0]);
+        double b = Double.parseDouble(expression[2]);
+
+        double result;
+        switch (expression[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                return "Error";
+        }
+        if (expression.length < 4) {
+            return String.valueOf(result);
+        }
+        double c = Double.parseDouble(expression[4]);
+        switch (expression[3]) {
+
+            case "+":
+                result = result + c;
+                break;
+            case "-":
+                result = result - c;
+                break;
+            case "*":
+                result = result * c;
+                break;
+            case "/":
+                result = result / c;
+                break;
+            default:
+                return "Error";
+
+
+        }
+
+        return String.valueOf(result);
     }
 
 }
