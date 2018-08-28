20e75475eed3b734b499012f99b55c54ecd85498
Dmitry <dmitry.osichkin@gmail.com>
2018-08-28 18:46:59 +0300

    23.08.2018 - calculate 2 or 3 operations

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.util.function.DoubleBinaryOperator;
 
 /**
  * @author Dimitry Osichkin
@@ -10,54 +9,28 @@ import java.util.function.DoubleBinaryOperator;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        double a = Double.parseDouble(expression[0]);
-        double b = Double.parseDouble(expression[2]);
 
-        double result;
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
-                return "Error";
-        }
-        if (expression.length < 4) {
-            return String.valueOf(result);
-        }
-        double c = Double.parseDouble(expression[4]);
-        switch (expression[3]) {
-
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            case "*":
-                result = result * c;
-                break;
-            case "/":
-                result = result / c;
-                break;
-            default:
-                return "Error";
-
-
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "Error";
+            }
         }
 
         return String.valueOf(result);
     }
-
 }
+
+
+
+
+
